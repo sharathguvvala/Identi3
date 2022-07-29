d16e3ab8a7fdefbc0e2791a621be3baf74015568
sharathguvvala <guvvalasharath@gmail.com>
2022-07-29 18:17:00 +0530

    hydration

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -1,18 +1,34 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div class="float-right mr-20 mt-5">
       <Breadcrumb spacing="20px">
         <BreadcrumbItem>
-          <BreadcrumbLink class="no-underline">
-            <ConnectButton />
+          <ConnectButton />
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink>
+            <Link href="/issuecredentials">
+              <a class="no-underline">Issue Credential</a>
+            </Link>
           </BreadcrumbLink>
         </BreadcrumbItem>
-
         <BreadcrumbItem>
-          <BreadcrumbLink class="no-underline">Profile</BreadcrumbLink>
+          <BreadcrumbLink>
+            <Link href="/verifycredentials">
+              <a class="no-underline">Verify Credential</a>
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink>
+            <Link href="/profile">
+              <a class="no-underline">Profile</a>
+            </Link>
+          </BreadcrumbLink>
         </BreadcrumbItem>
       </Breadcrumb>
     </div>
